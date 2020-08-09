04788cc2cb1e10e4123fb6f6495de96802c47cdf
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-08-09 09:13:45 +0530

    MLStreamLitApp - Initial Development
    MLStreamLitApp - Initial Development

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+
+def main():
+    st.title("Hello Streamlit")
+    st.subheader("Welcome to my first streamlit app")
+    st.text("Some text here")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
