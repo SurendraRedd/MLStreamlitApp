b91a4fdd583236fb07bf5ddbcdc82aefb816d472
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-08-30 12:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-st.title("Hello Streamlit")
-st.subheader("Welcome to my first streamlit app")
-st.text("Hosting it to Heroku")
+def main():
+    st.title("Hello Streamlit")
+    st.subheader("Welcome to my first streamlit app")
+    st.text("Deploy the Application in Heroku")
+
+
+
+if __name__ == "__main__":
+    main()
